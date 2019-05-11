b56fdd10c3eca12b5aa713fd098f2afecd666585
Mip Map <mipmap@gmail.com>
2019-05-11 10:28:31 -0400

    Translate comments to english

--- src/js/td-element.js
+++ src/js/td-element.js
@@ -5,8 +5,8 @@
  * Blog: http://oldj.net/
  *
  *
- * 本文件定义了 Element 类，这个类是游戏中所有元素的基类，
- * 包括地图、格子、怪物、建筑、子弹、气球提示等都基于这个类
+ * This file defines the Element class, which is the base class for all elements in the game.
+ * Includes maps, grids, monsters, buildings, bullets, balloon tips, etc. based on this class 
  *
  */
 
@@ -15,9 +15,9 @@
 _TD.a.push(function (TD) {
 
 	/**
-	 * Element 是游戏中所有可控制元素的基类
-	 * @param id {String} 给这个元素一个唯一的不重复的 ID，如果不指定则随机生成
-	 * @param cfg {Object} 元素的配置信息
+	 * Element is the base class for all controllable elements in the game
+	 * @param id {String} Give this element a unique, non-repeating ID, randomly generated if not specified
+	 * @param cfg {Object} Element configuration information
 	 */
 	TD.Element = function (id, cfg) {
 		this.id = id || ("el-" + TD.lang.rndStr());
@@ -43,33 +43,33 @@ _TD.a.push(function (TD) {
 			var _this = this,
 				i, en, len;
 
-			// 监听指定事件
+			// Listen for the specified event
 			for (i = 0, len = this.on_events.length; i < len; i++) {
 				en = this.on_events[i];
 				switch (en) {
 
-					// 鼠标进入元素
+					// Mouse into the element
 					case "enter":
 						this.on("enter", function () {
 							_this.onEnter();
 						});
 						break;
 
-					// 鼠标移出元素
+					// Mouse out of the element
 					case "out":
 						this.on("out", function () {
 							_this.onOut();
 						});
 						break;
 
-					// 鼠标在元素上，相当于 DOM 中的 onmouseover
+					// The mouse is on the element, which is equivalent to the onmouseover in the DOM
 					case "hover":
 						this.on("hover", function () {
 							_this.onHover();
 						});
 						break;
 
-					// 鼠标点击了元素
+					// Mouse clicked on the element
 					case "click":
 						this.on("click", function () {
 							_this.onClick();
@@ -80,13 +80,13 @@ _TD.a.push(function (TD) {
 			this.caculatePos();
 		},
 		/**
-		 * 重新计算元素的位置信息
+		 * Recalculate the location information of an element
 		 */
 		caculatePos: function () {
-			this.cx = this.x + this.width / 2; // 中心的位置
+			this.cx = this.x + this.width / 2; // Center location
 			this.cy = this.y + this.height / 2;
-			this.x2 = this.x + this.width; // 右边界
-			this.y2 = this.y + this.height; // 下边界
+			this.x2 = this.x + this.width; // Right boundary
+			this.y2 = this.y + this.height; // Lower boundary
 		},
 		start: function () {
 			this.is_paused = false;
@@ -102,13 +102,13 @@ _TD.a.push(function (TD) {
 			this.is_visiable = true;
 		},
 		/**
-		 * 删除本元素
+		 * Delete this element
 		 */
 		del: function () {
 			this.is_valid = false;
 		},
 		/**
-		 * 绑定指定类型的事件
+		 * Bind an event of the specified type
 		 * @param evt_type {String} 事件类型
 		 * @param f {Function} 处理方法
 		 */
@@ -116,7 +116,7 @@ _TD.a.push(function (TD) {
 			TD.eventManager.on(this, evt_type, f);
 		},
 
-		// 下面几个方法默认为空，实例中按需要重载
+		// The following methods are empty by default, and the instances are overloaded as needed.
 		onEnter: TD.lang.nullFunc,
 		onOut: TD.lang.nullFunc,
 		onHover: TD.lang.nullFunc,
@@ -125,9 +125,9 @@ _TD.a.push(function (TD) {
 		render: TD.lang.nullFunc,
 
 		/**
-		 * 将当前 element 加入到场景 scene 中
-		 * 在加入本 element 之前，先加入 pre_add_list 中的element
-		 * @param scene
+		 * Add the current element to the scene
+		 * Join the element in pre_add_list before adding this element
+         * @param scene
 		 * @param step_level {Number}
 		 * @param render_level {Number}
 		 * @param pre_add_list {Array} Optional [element1, element2, ...]
